b2371946bd56bdf56c71df0aa3fdaf69fd59c653
pessimist101 <issythegirl@gmail.com>
2020-07-26 02:26:28 +0100

    feat/Fleshed out substitution loop

--- build.py
+++ build.py
@@ -1,3 +1,9 @@
 import os
 import json
 
+with open('redirects.json','r') as f:
+    redirects = f.read()
+
+for i in redirects['redirectRules']:
+    block = f"""if {i['conditions']} {{{i['transformation']};
+    return Response.redirect(location, 301);}}"""
